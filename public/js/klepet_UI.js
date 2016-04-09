38c6541b32dfc5c7c1ba08e3d9a65f10ab252ab0
zlogar21 <zlogar21@gmail.com>
2016-04-09 21:14:38 +0000

    Zaključek dela na I. nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -99,6 +99,10 @@ $(document).ready(function() {
     for (var i=0; i < uporabniki.length; i++) {
       $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
     }
+    $('#seznam-uporabnikov div').click(function() {
+    $('#poslji-sporocilo').val('/zasebno "'+$(this).text()+'" ');
+    $('#poslji-sporocilo').focus();
+    });
   });
 
   setInterval(function() {
